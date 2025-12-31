022b5814ff9ad5647fee4f14428a26b555d7d481
Minxuan-Hu <74728864+Minxuan-Hu@users.noreply.github.com>
2025-12-31 15:21:46 -0500

    Create params.py

---
+++ metrics/params.py
@@ -0,0 +1,7 @@
+import torch.nn as nn
+
+
+def count_parameters(model: nn.Module, trainable_only: bool = True) -> int:
+    if trainable_only:
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return sum(p.numel() for p in model.parameters())
